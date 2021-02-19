6019d62cf8515842f539b027aecf6db23d3fb381
Jacob Parsell <jacobp@redsson.com>
2021-02-19 13:25:15 -0700

    update to work with current fluree_wrapper

--- template/src/App.js
+++ template/src/App.js
@@ -1,34 +1,64 @@
 import React from 'react'
 import logo from './logo.svg';
 import './App.css';
-import { ReactConnect, FlureeProvider, flureeQL } from '@fluree/react-wrapper';
-import CollectionTable from './CollectionTable';
+import { FlureeConn, FlureeProvider, useFlureeQuery } from '@fluree/react-wrapper';
 
 
 // import '@fluree/react-wrapper/dist/index.css'
-const myconn = ReactConnect({
+const myconn = new FlureeConn({
   servers: "http://localhost:8090",
   ledger: "example/ledger",
-  workerUrl: "flureeworker.js",
-  private: null
+  workerUrl: "/flureeworker.js"
 });
 
+const query = {
+  select: [
+    "*"
+  ],
+  from: "_collection"
+}
+
+function ShowCollections(){
+  const data = useFlureeQuery(query);
+  const { result, loading } = data;
+  if (loading) {
+    return (<div>Loading....</div>);
+  } else {
+  return (
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Doc</th>
+              <th>Version</th>
+            </tr>
+          </thead>
+          <tbody>
+            {result.map(collection => (
+              <tr key={collection._id}>
+                <td>{collection._id}</td>
+                <td>{collection.name}</td>
+                <td>{collection.doc}</td>
+                <td>{collection.version}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+  )
+  };
+}
 
 const App = () => {
   // return <ExampleComponent text="Create React Library Example 😄" />
   return (
     <FlureeProvider conn={myconn}>
-      <div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Fluree! The sample below uses a generic CollectionTable component to query for any collection.</p>
-        <p>Predicates in your ledger are:</p>
-        <CollectionTable
-          collection="_predicate"
-          predicates={["name", "doc", "type", "unique", "multi"]}
-        />
-      </div>
-    </FlureeProvider >
-  );
+      <ShowCollections />
+    </FlureeProvider>
+  )
 }
 
 export default App
